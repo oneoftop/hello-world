e1e04a5b8697639b7ba7709d5102d9be2da94779
doyoungkim <doyoungkim@10.211.55.4>
2017-01-05 18:04:56 +0900

    Signed-off-by: doyoungkim <doyoungkim@10.211.55.4>

--- HelloWorld/src/com/test/TestServlet.java
+++ HelloWorld/src/com/test/TestServlet.java
@@ -4,7 +4,7 @@ public class TestServlet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("12345");
+		System.out.println("1234567");
 	}
 
 }
